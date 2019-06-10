6d675091baea1d48937da5241df9ca214aad9eee
5c0tt411en <5c0tt.411en@gmail.com>
2019-06-10 20:38:10 +0900

    Initial commit

---
+++ SOPtoSVG/dep/sop-to-svg/update-dep-python-windows.cmd
@@ -0,0 +1,5 @@
+:: Update dependencies
+:: make sure pip is up to date
+python -m pip install --upgrade pip
+:: install requirements
+pip install -r C:/Users/scott/Documents/Derivative/TD_PROJECT/tauInfographicsExercise2019/SOPtoSVG/dep/sop-to-svg/requirements.txt --target="C:/Users/scott/Documents/Derivative/TD_PROJECT/tauInfographicsExercise2019/SOPtoSVG/dep/python"
